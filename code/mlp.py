2fc1d5c4b93ef4e339de62ba627908e1520d6aa5
Albert Johannessen <albertjohannessen@live.no>
2025-04-21 02:42:21 +0200

    node

--- code/mlp.py
+++ code/mlp.py
@@ -1,8 +1,9 @@
 from typing import Dict, List, Sequence
 
+import diffrax
 import jax
 import jax.numpy as jnp
-from jaxtyping import Array
+from jaxtyping import Array, Float, PyTree
 
 
 def mlp_init(layer_sizes: Sequence[int], key: Array) -> List[Dict[str, Array]]:
@@ -21,7 +22,7 @@ def mlp_init(layer_sizes: Sequence[int], key: Array) -> List[Dict[str, Array]]:
     return params
 
 
-def mlp_forward(x: Array, params: List[Dict[str, Array]]):
+def mlp_forward(x: Float[Array, "batch dim"], params: List[Dict[str, Array]]):
     for i in range(len(params)):
         weights = params[i]["weights"]
         bias = params[i]["bias"]
@@ -29,3 +30,53 @@ def mlp_forward(x: Array, params: List[Dict[str, Array]]):
         if i < len(params) - 1:
             x = jnp.tanh(x)
     return x
+
+
+def node_forward(
+    x0: Float[Array, "batch dim"],
+    t: Float[Array, "time"],
+    dt0: Float,
+    params: List[Dict[str, Array]],
+) -> Float[PyTree, "time batch dim"]:
+    T0 = t[0]
+    T1 = t[-1]
+    args = params
+
+    term = diffrax.ODETerm(lambda t, x, args: mlp_forward(x, args))
+    solver = diffrax.Tsit5()
+    saveat = diffrax.SaveAt(ts=t)
+    stepsize_controller = diffrax.PIDController(rtol=1e-4, atol=1e-6)
+    adjoint = diffrax.RecursiveCheckpointAdjoint()
+    # adjoint = diffrax.BacksolveAdjoint(solver=solver)
+
+    solution = diffrax.diffeqsolve(
+        term,
+        solver,
+        t0=T0,
+        t1=T1,
+        dt0=dt0,
+        y0=x0,
+        args=args,
+        saveat=saveat,
+        stepsize_controller=stepsize_controller,
+        adjoint=adjoint,
+    )
+    return solution.ys
+
+
+if __name__ == "__main__":
+    seed = 42069
+    key = jax.random.PRNGKey(seed)
+
+    key, subkey = jax.random.split(key)
+    params = mlp_init((2, 10, 10, 2), subkey)
+
+    batch_size = 10
+    dim = 2
+
+    key, subkey = jax.random.split(key)
+    x0 = jax.random.normal(subkey, (batch_size, dim))
+    t = jnp.arange(0.0, 4.0, 1.0)
+    dt0 = 0.01
+    x = node_forward(x0, t, dt0, params)
+    print(x.shape)
